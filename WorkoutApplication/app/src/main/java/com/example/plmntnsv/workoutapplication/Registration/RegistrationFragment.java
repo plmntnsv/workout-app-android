401beef8be8672c9a01a2d0b8e2a3bc0fadb6322
Plamen <plamen.atanasov91@gmail.com>
2017-10-08 03:43:56 +0300

    added firebase register

--- WorkoutApplication/app/src/main/java/com/example/plmntnsv/workoutapplication/Registration/RegistrationFragment.java
+++ WorkoutApplication/app/src/main/java/com/example/plmntnsv/workoutapplication/Registration/RegistrationFragment.java
@@ -1,19 +1,31 @@
 package com.example.plmntnsv.workoutapplication.Registration;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.plmntnsv.workoutapplication.Home.HomeActivity;
 import com.example.plmntnsv.workoutapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegistrationFragment extends Fragment {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     public RegistrationFragment() {
         // Required empty public constructor
@@ -21,9 +33,62 @@ public class RegistrationFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-       View root = inflater.inflate(R.layout.fragment_registration, container, false);
+       final View root = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        final EditText registerEmail = root.findViewById(R.id.et_registerEmail);
+        final EditText registerPassword = root.findViewById(R.id.et_registerPassword);
+        final EditText registerPasswordRepeat = root.findViewById(R.id.et_registerRepeatPassword);
+
+        Button registerBtn = root.findViewById(R.id.btn_register);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = registerEmail.getText().toString().trim();
+                String password = registerPassword.getText().toString().trim();
+                String repeatPassword = registerPasswordRepeat.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(root.getContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(root.getContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(root.getContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!(password.equals(repeatPassword))){
+                    Toast.makeText(root.getContext(),"Passwords don't match", Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(root.getContext(), "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(root.getContext(), "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Intent intent = new Intent(root.getContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+            }
+        });
+
         return root;
     }
 
---
+++ WorkoutApplication/app/src/main/java/com/example/plmntnsv/workoutapplication/utils/TextUtils.java
@@ -0,0 +1,16 @@
+package com.example.plmntnsv.workoutapplication.utils;
+
+/**
+ * Created by Plmn Tnsv on 08-Oct-17.
+ */
+
+public final class TextUtils {
+
+    public final boolean isEmpty(String str){
+        if (str.isEmpty()){
+            return true;
+        }
+
+        return false;
+    }
+}
